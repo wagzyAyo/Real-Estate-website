a8dcbdf1f43599b1f378ae728dc192e70efd6637
Ayo <talktojmcvibes@gmail.com>
2024-07-24 15:16:28 +0100

    get rent data

--- app.js
+++ app.js
@@ -5,11 +5,28 @@ async function getSales() {
             return 'No data available'
         }
         const data = await response.json();
-        //return json
+        //return data
         console.log(data)
     } catch (err) {
         console.log(err)
     }
 }
 
-getSales()
+getSales();
+
+async function getRent(){
+    try {
+        const response = await fetch("http://localhost:3000/api/rent");
+    if(response.status !== 200){
+        return "No data availbale check back later"
+    }
+    const data = await response.json();
+    // return data
+    console.log(data)
+    } catch (err) {
+        console.log(err)
+    }
+    
+}
+
+getRent();
