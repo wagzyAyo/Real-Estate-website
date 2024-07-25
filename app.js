724521650841ea2ca71feceffdf35cc84a97b3ea
Ayo <talktojmcvibes@gmail.com>
2024-07-25 14:19:52 +0100

    get leasedata from server and render in html

--- app.js
+++ app.js
@@ -156,3 +156,58 @@ async function displayRent(){
     })
 }
 displayRent();
+
+async function displayLease(){
+    const leaseData = await getRent()
+    const leaseCardContainer = document.querySelector(".lease-props");
+
+    leaseData.forEach(data =>{
+        //create new card
+        const card = document.createElement("div");
+        card.classList.add("prop-tab");
+    
+        //create and set content of the image
+        const imgDiv = document.createElement("div");
+        imgDiv.classList.add("prop-img");
+        const img = document.createElement("img");
+        img.src = data.imageURL;
+        img.alt = "";
+        img.style.width = "232px";
+        img.style.height = "184px";
+        img.style.borderRadius = "20px 20px 0 0";
+        img.style.objectFit = "cover"
+        imgDiv.appendChild(img)
+    
+        //create and set content for the feature
+        const featuresDiv = document.createElement("div");
+        featuresDiv.classList.add("features");
+        const nameDiv = document.createElement("div");
+        nameDiv.classList.add()
+        const priceDiv = document.createElement("div");
+        priceDiv.classList.add("price");
+        priceDiv.innerHTML = `<span>#</span>${data.amount}`;
+        const line1 = document.createElement("div");
+        line1.classList.add("line");
+        const sizeP = document.createElement("p");
+        sizeP.textContent = data.size;
+        const line2 = document.createElement("div");
+        line2.classList.add("line");
+        const locationB = document.createElement("b");
+        locationB.textContent = data.location;
+        featuresDiv.append(priceDiv, line1, sizeP, line2, locationB);
+    
+        // Create and set the content for the details
+        const detailsDiv = document.createElement("div");
+        detailsDiv.classList.add("details");
+        const detailsP = document.createElement("p");
+        detailsP.textContent = data.short;
+        detailsDiv.appendChild(detailsP);
+    
+        // Append all created elements to the card
+        card.append(imgDiv, featuresDiv, detailsDiv);
+    
+        // Append the card to the container
+        leaseCardContainer.appendChild(card);
+    })
+}
+displayRent();
